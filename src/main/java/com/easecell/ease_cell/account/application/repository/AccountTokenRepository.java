89c6f2718ea9d34b057aad5f4d20a5221ebfcc30
Giovanni Marques <giovanni.marques030@gmail.com>
2024-12-19 09:43:21 -0300

    fix: remove account token repository

--- src/main/java/com/easecell/ease_cell/account/application/repository/AccountTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.easecell.ease_cell.account.application.repository;
-
-import com.easecell.ease_cell.account.domain.entity.ResetToken;
-
-public interface AccountTokenRepository {
-  void save(ResetToken resetToken);
-}
